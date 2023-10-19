28419fd617b6ecef6666182a430cccfd1843fe4a
Dilanapm <apolacadilan@gmail.com>
2023-10-19 11:35:53 -0400

    segundo componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 import "./App.css";
 import { Perfil } from './assets/Perfil.jsx'
+import { Backp } from "./assets/Backp";
 function App() {
   return (
   <body>
+    <Backp></Backp>
     <Perfil></Perfil>
   </body>
   );
---
+++ src/assets/Backp.jsx
@@ -0,0 +1,7 @@
+export function Backp() {
+  return(
+<div>
+<h1>hola back</h1>
+</div>
+)
+}
